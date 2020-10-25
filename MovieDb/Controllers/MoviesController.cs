0ed2bd476403db70ca7eb0bd435bc0215c79c428
inderdeol <Inder94638@gmail.com>
2020-10-26 00:55:28 +0300

    Movies controller created

---
+++ MovieDb/Controllers/MoviesController.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MovieDb.Data;
+using MovieDb.Models;
+
+namespace MovieDb.Controllers
+{
+    [Authorize]
+    public class MoviesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public MoviesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        // GET: Movies
+        public ActionResult Index()
+        {
+            var movies = _context.Movies.ToList();
+            return View(movies);
+        }
+
+        // GET: Movies/Details/5
+        public ActionResult Details(int id)
+        {
+            var movie = _context.Movies.Include(x => x.Casts).FirstOrDefault(x => x.Id == id);
+            return View(movie);
+        }
+
+        // GET: Movies/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Movies/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([FromForm] Movie movie)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _context.Movies.Add(movie);
+                    _context.SaveChanges();
+                }
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Movies/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var movie = _context.Movies.FirstOrDefault(x => x.Id == id);
+            return View(movie);
+        }
+
+        // POST: Movies/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, [FromForm] Movie movie)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _context.Update(movie);
+                    _context.SaveChanges();
+                }
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View(movie);
+            }
+        }
+
+        // GET: Movies/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var movie = _context.Movies.Include(x => x.Casts).FirstOrDefault(x => x.Id == id);
+            return View(movie);
+        }
+
+        // POST: Movies/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, [FromForm] Movie movie)
+        {
+            try
+            {
+                _context.Remove(movie);
+                _context.SaveChanges();
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View(movie);
+            }
+        }
+    }
+}
--- MovieDb/Data/ApplicationDbContext.cs
+++ MovieDb/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using MovieDb.Models;
 
 namespace MovieDb.Data
 {
@@ -12,5 +13,7 @@ namespace MovieDb.Data
             : base(options)
         {
         }
+        public virtual DbSet<Movie> Movies { get; set; }
+        public virtual DbSet<Cast> Casts { get; set; }
     }
 }

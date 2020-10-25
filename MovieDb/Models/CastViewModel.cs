da7cc1b17165cea1d6180f1bec523ad78473a834
inderdeol <Inder94638@gmail.com>
2020-10-26 00:51:10 +0300

    Movie and Cast view models created

---
+++ MovieDb/Models/CastViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieDb.Models
+{
+    public class Cast
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Firstname { get; set; }
+        [Required]
+        public string Lastname { get; set; }
+        public Gender Gender { get; set; }
+        public string Biography { get; set; }
+        public int MovieId { get; set; }
+        public virtual Movie Movie { get; set; }
+    }
+}
---
+++ MovieDb/Models/MovieViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieDb.Models
+{
+    public class Movie
+    {
+        [Key]
+        public int Id { get; set; }
+        [MaxLength(50, ErrorMessage = "Movie Title can not exceed 50 characters.")]
+        [Required]
+        public string Title { get; set; }
+        public Genre Genre { get; set; }
+        public double Rating { get; set; }
+        [MaxLength(1024, ErrorMessage = "Movie Title can not exceed 1024 characters.")]
+        [Required]
+        public string Description { get; set; }
+        public int Year { get; set; }
+        public virtual ICollection<Cast> Casts { get; set; }
+    }
+}

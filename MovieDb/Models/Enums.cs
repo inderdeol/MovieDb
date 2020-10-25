f3bd223cbfb0a6cb9918bfa2137a67e547c6990d
inderdeol <Inder94638@gmail.com>
2020-10-26 00:49:04 +0300

    Genre and gender enums created

---
+++ MovieDb/Models/Enums.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieDb.Models
+{
+    public enum Genre
+    {
+        Action,
+        Comedy,
+        Drama,
+        Horror,
+        Fiction,
+        Romance
+    }
+
+    public enum Gender
+    {
+        Female,
+        Male
+    }
+}
